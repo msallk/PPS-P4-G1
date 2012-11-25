1152c68f41348c541a6259d11bcc04476bae0907
Jiang Wu <tiberia.sun@gmail.com>
2012-11-24 20:17:22 -0500

    add signature for path searching

--- cell/g1/Graph.java
+++ cell/g1/Graph.java
@@ -1,5 +1,20 @@
 package cell.g1;
 
+import java.util.ArrayList;
+
 public class Graph {
+	private int[][] map;
+
+	public Graph(int[][] map){
+		this.map=map;
+	}
+
+	public ArrayList<Path> getPath(int[] current, int[] dest){
+		return null;
+	}
+	
+	private void BFS(int[] current, int[] dest){
+		
+	}
 
 }
---
+++ cell/g1/Path.java
@@ -0,0 +1,18 @@
+package cell.g1;
+
+import java.util.ArrayList;
+
+public class Path implements Comparable{
+	int length;
+	ArrayList<Integer[]> locs;
+	
+	public void add(int[] loc){
+		
+	}
+
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
